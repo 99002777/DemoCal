4574022166e4055b05e9e0f925387b126f340369
99002777 <72496952+99002777@users.noreply.github.com>
2020-10-08 17:42:04 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
+#include<math.h>
+#include<lib.h>
 int main()
 {
-  int a;
-  printf("Enter number");
-  scanf("%d",&a);
-  void power(int);
-  void expo(int);
+  //int a;
+  //printf("Enter number");
+  //scanf("%d",&a);
+  void power();
+  void exponential();
   return 0;
 }
